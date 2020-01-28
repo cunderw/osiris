07febbf623f3a08c5f9fcc1a60ac3a3c4c1fb489
Carson Underwood <cunderw@sonos.annacar.local>
2020-01-28 14:56:06 -0600

    OSIRIS-11 continuing on menu POC. Dynmic scroll list and some better graphics added

---
+++ Assets/_Scripts/UI/ItemListButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemListButton : MonoBehaviour {
+
+    [SerializeField]
+    private Text buttonText;
+    public void SetText(string textString) {
+        buttonText.text = textString;
+    }
+
+    public void OnClick() {
+
+    }
+}
---
+++ Assets/_Scripts/UI/ItemListController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemListController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    void Start() {
+        BuildList();
+    }
+
+    private void BuildList() {
+        for (int i = 0; i < 20; i++) {
+            Debug.Log("Adding Button " + 1);
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+
+            button.SetActive(true);
+
+            button.GetComponent<ItemListButton>().SetText("Item - " + i);
+
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+        }
+
+    }
+
+}
