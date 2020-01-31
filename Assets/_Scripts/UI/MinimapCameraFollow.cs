c5c797a103685f774025b68df2d1b29a4704681c
Bronson Cox <bronsoncox@hotmail.com>
2020-01-30 23:42:40 -0600

    minimap/render texture poc, fixed SceneLoader error

---
+++ Assets/_Scripts/UI/MinimapCameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _Scripts.UI
+{
+    public class MinimapCameraFollow : MonoBehaviour
+    {
+        [SerializeField] private GameObject playerToFollow;
+
+        private void Start()
+        {
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+            SetMiniMapCamera();
+        }
+
+
+        private void FixedUpdate()
+        {
+            SetMiniMapCamera();
+        }
+
+        private void SetMiniMapCamera()
+        {
+            transform.position = playerToFollow.transform.position + new Vector3(0, 10, 0);
+        }
+    }
+}
