34681cbbafb24de70552d4764e20db35e24dfc95
CARSON UNDERWOOD <cunderw@gmail.com>
2020-02-04 12:36:38 -0600

    OSIRIS-11 Silders now control the mixer volumes, need to hook up storing it

--- Assets/_Scripts/Menus/CharacterInfoMenu/SettingsAudioController.cs
+++ Assets/_Scripts/Menus/CharacterInfoMenu/SettingsAudioController.cs
@@ -1,27 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsAudioController : MonoBehaviour {
 
     [SerializeField]
     private GameObject musicSlider, sfxSlider, voiceSlider;
+    public AudioMixer mixer;
     void start() {
         // TODO - Set from saved value
+        musicSlider.GetComponent<Slider>().value = mixer.GetFloat("MusicVolume")
     }
-    public void MusicSlider() {
-        float volume = musicSlider.GetComponent<Slider>().value;
+    public void MusicSlider(float volume) {
+        //float volume = musicSlider.GetComponent<Slider>().value;
         Debug.Log("[SettingsAudioController] -  Setting Music Volume To: " + volume);
-    }
-
-    public void SoundEffectsSlider() {
-        float volume = sfxSlider.GetComponent<Slider>().value;
-        Debug.Log("[SettingsAudioController] -  Setting Music Volume To: " + volume);
+        mixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
     }
 
-    public void VoiceSlider() {
-        float volume = voiceSlider.GetComponent<Slider>().value;
+    public void SoundEffectsSlider(float volume) {
         Debug.Log("[SettingsAudioController] -  Setting Music Volume To: " + volume);
+        mixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void VoiceSlider(float volume) {
+        Debug.Log("[SettingsAudioController] -  Setting Music Volume To: " + volume);
+        mixer.SetFloat("VoiceVolume", Mathf.Log10(volume) * 20);
     }
 }
