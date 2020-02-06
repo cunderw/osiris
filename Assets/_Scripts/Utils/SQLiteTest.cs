40350b765b0e2f02ae1246c68db31a6ab14bd11b
CARSON UNDERWOOD <cunderw@gmail.com>
2020-02-06 16:03:49 -0600

    adding sqlite support for table drops and such

---
+++ Assets/_Scripts/Utils/SQLiteTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using System.IO;
+
+using Mono.Data.Sqlite;
+
+using UnityEngine;
+
+public class SQLiteTest : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        // Create database
+        Debug.Log(Application.streamingAssetsPath);
+        string connection = "URI=file:" + Application.streamingAssetsPath + "/My_Database";
+
+        // Open connection
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        // Create table
+        IDbCommand dbcmd;
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable = "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER )";
+
+        dbcmd.CommandText = q_createTable;
+        dbcmd.ExecuteReader();
+
+        // Insert values in table
+        IDbCommand cmnd = dbcon.CreateCommand();
+        cmnd.CommandText = "INSERT INTO my_table (id, val) VALUES (0, 5)";
+        cmnd.ExecuteNonQuery();
+
+        // Read and print all values in table
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        IDataReader reader;
+        string query = "SELECT * FROM my_table";
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader();
+
+        while (reader.Read()) {
+            Debug.Log("id: " + reader[0].ToString());
+            Debug.Log("val: " + reader[1].ToString());
+        }
+
+        // Close connection
+        dbcon.Close();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
