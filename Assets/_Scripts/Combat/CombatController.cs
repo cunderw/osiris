edef6d2e884a9aa64f55e43b1a1f3c4473af1ba2
Bronson Cox <bronsoncox@hotmail.com>
2020-02-08 16:04:44 -0600

    first pass on loading battle screen and enemies

---
+++ Assets/_Scripts/Combat/CombatController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _Scripts.Utils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+namespace _Scripts.Combat {
+    public class CombatController : MonoBehaviour {
+        //TODO: Initialize a list of gameobject enemy types at scene load to be able to assign to script
+        [SerializeField] private GameObject enemyType01;
+        private static Dictionary<GameObject, float> _initiativeDictionary = new Dictionary<GameObject, float>();
+        private Scene _currentScene;
+        private static float _initiativeRoll;
+
+        private void Start() {
+            _currentScene = SceneManager.GetActiveScene();
+            LoadUnloadBattleScene(true);
+            EnemyTypesToInstantiate(2, enemyType01);
+            foreach (var enemy in _initiativeDictionary) {
+                Debug.Log(enemy);
+            }
+        }
+
+        public void LoadUnloadBattleScene(bool battling) {
+            if(battling) {
+                SceneLoader.Load(SceneLoader.Scene.WeMustKungFuFight);
+            }
+            else SceneLoader.LoadScene(_currentScene);
+        }
+        
+        public static void EnemyTypesToInstantiate(int amount, GameObject enemyType01) {
+            // external type(s) passed from enemy gameobject in previous scene, null for Type02 possible
+            for(var enemy = 1; enemy <= amount; enemy++)
+            {
+                // Instantiate enemy of type enemyID from table and space evenly on screen from center
+                // Rolls init and adds to _initiativeList
+                //TODO: fix the math so enemies align Y axis from center of screen
+                GameObject enemyCount = Instantiate(enemyType01, new Vector3(enemy + 1.0f, 0, 0), Quaternion.identity);
+                _initiativeRoll = Random.Range(0.0f, 1.0f);
+                _initiativeDictionary.Add(enemyCount, _initiativeRoll);
+                DontDestroyOnLoad(enemyCount);
+            }
+        }
+
+        public static void CharactersToInstantiate(int amount) {
+            for (var partyMember = 1; partyMember < amount; partyMember++) {
+                // Pulls list of characters from current party roster and loads to list
+                // Rolls init and adds to _initiativeList
+                // Character with highest init centers to screen for first action
+            }
+        }
+
+        //if enemy within distance of player
+
+        //{
+        //    Initiative float 0-1
+        //    splits between enemies on screen and characters in party
+        //        modified based on dex + equip upgrades?
+        //}
+
+        //attack loop
+        //{
+        //   initiative 1
+        //    do thing (attack, heal, special, item)
+        //    start cooldown
+        //    go to 2, 3, etc + repeat
+        //}    
+
+        //when battle ends, clear initiative table
+
+        //pass to loot system    
+    }
+}
--- Assets/_Scripts/Utils/SceneLoader.cs
+++ Assets/_Scripts/Utils/SceneLoader.cs
@@ -5,17 +5,32 @@ using UnityEngine.SceneManagement;
 namespace _Scripts.Utils {
     public static class SceneLoader {
 
+        //TODO: populate at buildtime with SceneUtility.GetScenePathByBuildIndex or similar(?)
         public enum Scene {
 
             Loading,
             MainMenu,
             CustomizeCharacter,
+            WeMustKungFuFight,
             Tutorials_002,
             Chapter_001
         }
 
         private static Action onLoaderCallback;
         public static void Load(Scene scene) {
+            string sceneString = scene.ToString();
+            Debug.Log($"[SceneLoader] Loading Scene {sceneString}");
+
+            // Set the loader callback action to load the target scene
+            onLoaderCallback = () => {
+                SceneManager.LoadScene(sceneString);
+            };
+            // Load the loading scene
+            SceneManager.LoadScene(Scene.Loading.ToString());
+        }
+
+        //TODO: fix this one, added to pass current scene in CombatController
+        public static void LoadScene(UnityEngine.SceneManagement.Scene scene) {
             string sceneString = scene.ToString();
             Debug.Log($"[SceneLoader] Loading Scene {sceneString}");
 
